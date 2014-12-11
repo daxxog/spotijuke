45d879cc9c25920bf9136fa6dbf3263d8f87c75a
David Volm <david@volminator.com>
2014-12-11 17:35:38 -0600

    spotijuke reboot2

---
+++ make.js
@@ -0,0 +1,33 @@
+/* Spotijuke / make.js
+ * echo 'make script for Spotijuke' && node make
+ * (c) 2014 David (daXXog) Volm ><> + + + <><
+ * Released under Apache License, Version 2.0:
+ * http://www.apache.org/licenses/LICENSE-2.0.html  
+ */
+
+var bitfactory = require('bitfactory'),
+    UglifyJS = require("uglify-js"),
+    stoptime = require('stoptime'),
+    fs = require('fs');
+
+var watch = stoptime(),
+    header = '';
+
+bitfactory.make({ //routes
+    "": function(err, results) {
+        console.log('built Spotijuke in ' + watch.elapsed() + 'ms.');
+    }
+}, { //dependencies
+    "*": { //wildcard
+        "header": function(cb) {
+            fs.readFile('spotijuke.h', 'utf8', function(err, data) {
+                header = data;
+                cb(err);
+            });
+        },
+        "spotijuke.min.js": ["header", function(cb) {
+            fs.writeFileSync('spotijuke.min.js', header + UglifyJS.minify('spotijuke.js').code);
+            cb();
+        }]
+    }
+});
